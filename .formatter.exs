ba952884763645bdff173a0e88a23d953be25da8
Simon Lally <simonlallydev@gmail.com>
2023-02-04 22:25:50 -0500

    learning elixir and phoenix and having a great time, also tailwind is neat

---
+++ .formatter.exs
@@ -0,0 +1,6 @@
+[
+  plugins: [Phoenix.LiveView.HTMLFormatter],
+  import_deps: [:ecto, :phoenix],
+  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
+  subdirectories: ["priv/*/migrations"]
+]
---
+++ lib/hello_web/controllers/page_controller.ex
@@ -0,0 +1,8 @@
+defmodule HelloWeb.PageController do
+  use HelloWeb, :controller
+
+  @spec index(Plug.Conn.t(), any) :: Plug.Conn.t()
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+end
